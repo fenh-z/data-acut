df74de220a9411ef7ecdc1cf189c1baebc6d4db9
zhangdx <zdongxiao@outlook.com>
2022-09-20 10:33:43 +0800

    新增boot-starter

---
+++ data-acut-boot-starter/src/main/java/com/acut/DataAcutBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.acut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAcutBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAcutBootStarterApplication.class, args);
+    }
+
+}
---
+++ data-acut-boot-starter/src/test/java/com/acut/DataAcutBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAcutBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
