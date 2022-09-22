e5bf3ef72327080e8a9c60072882f5829611ec28
zhangdx <zdongxiao@outlook.com>
2022-09-22 09:14:26 +0800

    新增模版方法

--- data-acut-dependencies/src/main/java/com/acut/inter/Task.java
+++ data-acut-dependencies/src/main/java/com/acut/inter/Task.java
@@ -4,7 +4,8 @@ package com.acut.inter;
  * @className: Task
  * @author: zhangdx
  * @description: 规定task模版
- * @date: 2022/9/21 15:46
+ * @date:/ 2022/9/21 15:46
  */
 public interface Task {
+    public void execute();
 }
