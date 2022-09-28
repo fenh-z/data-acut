fd2b76cf30e5b4bd3403688858a25acc4664431e
zhangdx <zdongxiao@outlook.com>
2022-09-28 09:26:49 +0800

    新增调度管理，规定调度的基本参数

---
+++ data-acut-dependencies/src/main/java/com/acut/busi/ScheduleManager.java
@@ -0,0 +1,10 @@
+package com.acut.busi;
+
+/**
+ * @className: ScheduleManager
+ * @author: zhangdx
+ * @description: 调度管理，规定调度的基本参数
+ * @date: 2022/09/28 09:25
+ */
+public class ScheduleManager {
+}
